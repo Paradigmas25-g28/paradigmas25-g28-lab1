fd55a0a77d5a012b8b662ba3f72daefee50963fb
Lautaro Famaf <lautaro.deco@mi.unc.edu.ar>
2025-03-28 00:04:56 -0300

    Update Interp.hs
    Co-authored-by: TomasFT <tomasf.torres@mi.unc.edu.ar>

--- Interp.hs
+++ Interp.hs
@@ -1,4 +1,6 @@
 -- Sacar del esqueleto final!
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
 module Interp where
 import Graphics.Gloss
 import Graphics.Gloss.Data.Vector
@@ -13,30 +15,46 @@ type Interpretacion a = a -> ImagenFlotante
 mitad :: Vector -> Vector
 mitad = (0.5 V.*)
 
+productoEscalar :: Float -> Vector -> Vector
+productoEscalar k (x, y) = (k * x, k * y)
 -- Interpretaciones de los constructores de Dibujo
 
 --interpreta el operador de rotacion
 interp_rotar :: ImagenFlotante -> ImagenFlotante
-interp_rotar d w h = (rotate -90) d+h h -w
+interp_rotar imgf d w h = imgf (d+h) h (-w)
 
 --interpreta el operador de espejar
 interp_espejar :: ImagenFlotante -> ImagenFlotante
-interp_espejar d w h = (scale -1 1) d+w -w h
+interp_espejar imgf d w = imgf (d+w) (-w)
 
 --interpreta el operador de rotacion 45
 interp_rotar45 :: ImagenFlotante -> ImagenFlotante
-interp_rotar45 = (rotate 45) d w h
+interp_rotar45 imgf d w h = imgf (d+(w+h)/2) ((w+h)/2) ((h-w)/2)
 
 --interpreta el operador de apilar
-interp_apilar :: Int -> Int -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante
-
+interp_apilar :: Float -> Float -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante
+interp_apilar n m imgf1 imgf2 d w h =
+  pictures [
+    imgf1 ( d + mulSV (n / (n + m)) h) w (mulSV (m / (m + n)) h),
+    imgf2 d w (mulSV (n / (n + m)) h)
+  ]
 --interpreta el operador de juntar
-interp_juntar :: Int -> Int -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante
-
+interp_juntar :: Float -> Float -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante
+interp_juntar n m imgf1 imgf2 d w h =
+  pictures [
+    imgf1 d (mulSV (m / (n + m)) w) h,
+    imgf2 ( d + mulSV (m / (n + m)) w) (mulSV (n / (n + m)) w) h
+  ]
 --interpreta el operador de encimar
 interp_encimar :: ImagenFlotante -> ImagenFlotante -> ImagenFlotante
-
+interp_encimar imgf1 imgf2 a b c = pictures [imgf1 a b c, imgf2 a b c]
 --interpreta cualquier expresion del tipo Dibujo a
 --utilizar foldDib 
 interp :: Interpretacion a -> Dibujo a -> ImagenFlotante
-
+interp inter = foldDib inter
+                         (interp_rotar (inter d w h) )
+                         (interp_espejar inter )
+                         (interp_rotar45 inter )
+                         (interp_apilar n m inter inter )
+                         (interp_juntar n m inter inter )
+                         (interp_encimar inter inter )
