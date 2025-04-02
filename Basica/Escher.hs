2b7ce0c652e885381c69722dda0bda6ab93e059b
Lautaro Famaf <lautaro.deco@mi.unc.edu.ar>
2025-04-01 23:27:36 -0300

    fixed eshcer.hs and draws its figure

--- Basica/Escher.hs
+++ Basica/Escher.hs
@@ -20,13 +20,16 @@ dibujoT d =
    in (d ^^^ r1) ^^^ r2
 
 -- lado con nivel de detalle
+-- lado :: Int -> Dibujo Escher -> Dibujo Escher
+-- lado n d
+--   | n < 0 = Basica False -- no deberia pasar
+--   | n == 1 = cuarteto (Basica False) (Basica False) (Rotar d) d
+--   | n > 1 =
+--       let l = lado (n - 1) d
+--        in cuarteto l l (Rotar d) d
 lado :: Int -> Dibujo Escher -> Dibujo Escher
-lado n d
-  | n < 0 = Basica False -- no deberia pasar
-  | n == 1 = cuarteto (Basica False) (Basica False) (Rotar d) d
-  | n > 1 =
-      let l = lado (n - 1) d
-       in cuarteto l l (Rotar d) d
+lado 0 _ = Basica False
+lado n p = cuarteto (lado (n - 1) p) (lado (n - 1) p) (Rotar (dibujoT p)) (dibujoT p)
 
 -- esquina con nivel de detalle en base a la figura p
 esquina :: Int -> Dibujo Escher -> Dibujo Escher
@@ -38,14 +41,20 @@ esquina n d
        in cuarteto (esquina (n - 1) d) l (Rotar l) (dibujoU d)
 
 -- por suerte no tenemos que poner el tipo!
+-- noneto p q r s t u v w x =
+--   let pq = Juntar 2 1 p q
+--       pqr = Juntar 1 3 pq r
+--       st = Juntar 2 1 s t
+--       stu = Juntar 1 3 st u
+--       vw = Juntar 2 1 v w
+--       vwx = Juntar 1 3 vw x
+--    in Apilar 1 3 (Apilar 2 1 pqr stu) vwx
 noneto p q r s t u v w x =
-  let pq = Juntar 2 1 p q
-      pqr = Juntar 1 3 pq r
-      st = Juntar 2 1 s t
-      stu = Juntar 1 3 st u
-      vw = Juntar 2 1 v w
-      vwx = Juntar 1 3 vw x
-   in Apilar 1 3 (Apilar 2 1 pqr stu) vwx
+  Apilar
+    1
+    2
+    (Juntar 1 2 p (Juntar 1 1 q r))
+    (Apilar 1 1 (Juntar 1 2 s (Juntar 1 1 t u)) (Juntar 1 2 v (Juntar 1 1 w x)))
 
 -- el dibujo de Escher:
 escher :: Int -> Escher -> Dibujo Escher
